ed339d445edea6fdc3b1355fa91bed537fa52b62
Jeffrey Ke <jeffke613@gmail.com>
2020-05-28 13:21:31 -0700

    Added some demo game obstacles, and changed the game board from grid format to what the actual pac-man game board looks like. Also deleted some obselete code.

--- src/Game.java
+++ src/Game.java
@@ -14,6 +14,9 @@ import javafx.stage.Stage;
 public class Game extends Application {
 	private Pacman pac;
 	private Pacworld world;
+	private Ghost orange;
+	private Wall block_0;
+	private Wall block_1;
 	public Game() {
 		// TODO Auto-generated constructor stub
 	}
@@ -42,8 +45,26 @@ public class Game extends Application {
 //		}
 		pac.setX(51);
 		pac.setY(51);
+		
+//		orange = new Ghost("orange");
+//		world.add(orange);
+//		orange.setX(49);
+//		orange.setY(49);
+		
+		block_0 = new Wall(Game.class.getClassLoader().getResource("resources/smaller_block.png").toString());
+		block_0.setX(80);
+		block_0.setY(80);
+		world.add(block_0);
+		
+		block_1 = new Wall(Game.class.getClassLoader().getResource("resources/smaller_block.png").toString());
+		block_1.setX(160);
+		block_1.setY(80);
+		world.add(block_1);
+		
+		
+		
 		world.start();
-		String back_image = Game.class.getClassLoader().getResource("resources/dot_board.png").toString();
+		String back_image = Game.class.getClassLoader().getResource("resources/game_board.png").toString();
 		Image test = new Image(back_image);
 		BackgroundImage back = new BackgroundImage(new Image(back_image), BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
 		          BackgroundSize.DEFAULT);
--- src/Pacworld.java
+++ src/Pacworld.java
@@ -14,19 +14,7 @@ public class Pacworld extends World {
 	@Override
 	public void act(long now) {
 		// TODO Auto-generated method stub
-//		Stage stage = (Stage)this.getParent().getScene().getWindow();
-//		BorderPane root = new BorderPane();
-//		Pacworld world = new Pacworld();
-//		Pacman pac = new Pacman();
-//		pac.setX(400);
-//		pac.setY(300);
-//		world.add(pac);
-//		root.setCenter(world);
-//		world.start();
-//		Scene scene = new Scene(root);
-//		stage.setScene(scene);
-		
-//		world.start();
+
 	}
 
 }
